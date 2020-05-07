f4535a8550ea92b2fd067e2649f6f278c0c584b6
czhang2718 <55672656+czhang2718@users.noreply.github.com>
2020-05-07 01:33:38 -0400

    Update global.R

--- global.R
+++ global.R
@@ -8,3 +8,41 @@ vars<-dset_PTs$PT_TERM
 # page 2
 dset_SUBs <- read.csv("SUBs.csv")
 vars2<-dset_SUBs$INAME
+
+# collapse other boxes when a new one is opened
+collapseInput <- function(inputId, boxId) {
+  tags$script(
+    gsub("%s", boxId,
+      "$('#%s').closest('.box').on('hidden.bs.collapse', function () {
+        if('%s' === 'box1a') col_1a = true;
+        else if('%s' === 'box1b') col_1b = true;
+        else if('%s' === 'box1c') col_1c = true;
+      })
+      
+      
+      $('#%s').closest('.box').on('shown.bs.collapse', function () {
+        if('%s' === 'box1a') col_1a = false;
+        else if('%s' === 'box1b') col_1b = false;
+        else if('%s' === 'box1c') col_1c = false;
+        if(!col_1a){
+          if('%s' != 'box1a'){
+              $('#box1a').closest('.box').find('[data-widget=collapse]').click(); 
+              col_1a = true;
+            }
+        }
+        if(!col_1b){
+          if('%s' != 'box1b'){
+              $('#box1b').closest('.box').find('[data-widget=collapse]').click(); 
+              col_1b = true;
+            }
+        }
+        if(!col_1c){
+          if('%s' != 'box1c'){
+              $('#box1c').closest('.box').find('[data-widget=collapse]').click(); 
+              col_1c = true;
+            }
+        }
+      })
+      ")
+  )
+}
